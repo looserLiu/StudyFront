b74219fe95e318ecc153d04ef57e2db3bdd35055
looserLiu <13612007232@163.com>
2023-02-13 22:34:24 +0800

    初始化

---
+++ StudyFront/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page i34ff4nd4444e22222x</h1>
+    </div>
+  );
+}
